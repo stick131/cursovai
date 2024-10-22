b97f8684538439edb5ef269cac381987ccb87c0a
Egor <egorr316@gmail.com>
2024-10-22 21:29:01 +0300

    написани юнит тести

--- js/reg.js
+++ js/reg.js
@@ -1,9 +1,11 @@
 function reg(person, message){
     if(person.name.length == 0 || person.email.length == 0){
         message.text = "Ви не запонили усi даннi спробуйте ще раз";
-    }else if(person.name.length !=0 && (person.email.indexOf("@gmail") == -1 && person.email.indexOf("@zac") == -1)){
+    }else if(person.name.length !=0 && person.name.length <= 20 && (person.email.indexOf("@gmail") == -1 && person.email.indexOf("@zac") == -1)){
         message.title ="Антиспам захист";
-        message.text = "Будь ласка не використовуйте 10 хвилиннi пошти!"
+        message.text = "Будь ласка не використовуйте 10 хвилиннi пошти!";
+    }else if(person.name.length > 20){
+        message.text = "Ваше ім'я занадто довге, воно має бути менше 20 символів";
     }
 
 
--- js/script.js
+++ js/script.js
@@ -9,12 +9,13 @@ burger.addEventListener("click", (event)=>{
 
 document.querySelector(".footer-btn").addEventListener("click", (event) => {
     event.preventDefault();
-    let person = {
+    if(!event.target.classList.contains("write-btn")){
+    const person = {
         name: document.querySelector("#name").value,
         email: document.querySelector("#email").value 
     };
      
-    let message = {
+    const message = {
         title: document.querySelector(".popup__title").textContent,
         text: document.querySelector(".popup__text").textContent
     }
@@ -23,8 +24,21 @@ document.querySelector(".footer-btn").addEventListener("click", (event) => {
 
     document.querySelector(".popup__title").textContent = message.title;
     document.querySelector(".popup__text").textContent = message.text;
-
     document.querySelector(".overlay").classList.add("overlay--active");
+    }
+});
+
+document.querySelector(".write-btn").addEventListener("click", (event)=>{
+    event.preventDefault();
+    const person = {
+        name: document.querySelector("#name").value,
+        text: document.querySelector("#text").value
+    }
+    const message = {
+        title: document.querySelector(".popup__title").textContent,
+        text: document.querySelector(".popup__text").textContent
+    }
+
 });
 
 document.querySelector(".popup__close").addEventListener("click", (event) =>{
